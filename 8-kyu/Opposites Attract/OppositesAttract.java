36661ba367c23eea84741c3c8d3dc3e4dc02034d
Stefanus Adrian <stefanusj06@gmail.com>
2023-07-22 11:34:21 +0700

    added opposites attract

---
+++ 8-kyu/Opposites Attract/OppositesAttract.java
@@ -0,0 +1,8 @@
+package org.example.kyu_8.oppositesattract;
+
+public class OppositesAttract {
+
+    public static boolean isLove(final int flower1, final int flower2) {
+        return flower1 % 2 != flower2 % 2;
+    }
+}
