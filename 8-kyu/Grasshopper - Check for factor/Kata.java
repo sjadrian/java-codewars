67c2557b438410ce52e09313e32f1c605944ba77
Stefanus Adrian <stefanusj06@gmail.com>
2023-08-01 00:36:23 +0700

    added grasshopper - check for factor

---
+++ 8-kyu/Grasshopper - Check for factor/Kata.java
@@ -0,0 +1,7 @@
+package org.example.kyu_8.grasshoppercheckforfactor;
+
+public class Kata {
+    public static boolean checkForFactor(int base, int factor) {
+        return base % factor == 0;
+    }
+}
